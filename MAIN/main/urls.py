78f698c63badaf1bd30e9daa180e1de36bcbd3f2
ahsnl7 <ahsnl.mi@gmail.com>
2021-07-06 15:17:22 +0200

    change new theme

--- MAIN/main/urls.py
+++ MAIN/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home, post_detail, post_list
+from .views import home, post_detail, post_list, department_subjects, test_post_list
 urlpatterns = [
-    path("", home, name="home"),
-    path("posts/<slug>", post_list, name="posts"),
+    #path("", home, name="home"),
+    path("", department_subjects, name="home"),
+    path("posts/<slug>", test_post_list, name="posts"),
+    #path("posts/<slug>", post_list, name="posts"),
     path("detail/<slug>/", post_detail, name="detail"),
 ]
--- MAIN/main/views.py
+++ MAIN/main/views.py
@@ -4,6 +4,25 @@ from .models import Author, Comment, Post, Category
 from .utils import update_views
 
 
+def department_subjects(request):
+    forums = Category.objects.all()
+    context = {
+        "forums":forums,
+    }
+    return render(request, "screens/department_subjects.html", context)
+
+def test_post_list(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    posts = Post.objects.filter(approved=True, categories=category)
+
+    context = {
+        "posts": posts,
+        "forum": category,
+    }
+
+    return render(request, "screens/post_list.html", context)
+
+
 def home(request):
     forums = Category.objects.all()
     context = {
