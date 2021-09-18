c9d20f5f52c076f3c1d01023514228c81c574c82
ahsnl7 <ahsnl.mi@gmail.com>
2021-09-18 14:34:56 +0200

    add pg db to settings.py -- not utilize

--- MAIN/manage.py
+++ MAIN/manage.py
@@ -2,10 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv 
+import pathlib
 
 def main():
     """Run administrative tasks."""
+    DOT_ENV_PATH = pathlib.Path() / '.env'
+    if DOT_ENV_PATH.exists():
+        dotenv.read_dotenv(str(DOT_ENV_PATH))
+    else:
+        print("No .env could not be found or path is not specify")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
