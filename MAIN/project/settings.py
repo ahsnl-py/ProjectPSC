fd64ee21cfaebcee5aa75683db5c9b9a0d633d7a
ahsnl7 <ahsnl.mi@gmail.com>
2021-09-18 13:48:45 +0200

    change debug settings

--- MAIN/project/settings.py
+++ MAIN/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2%2+(rx8ytddo)m7w+*10h$^lp9brqbj@#0f$7-297u-o1@axo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
 ALLOWED_HOSTS = []
 
