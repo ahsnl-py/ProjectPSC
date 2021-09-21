4b3f3d506525711c9777e2aa2ee0bd3d7cfe63a6
Jahidul Alam <mjahidulalam@gmail.com>
2021-09-21 17:07:41 -0600

    database changes

--- MAIN/project/settings.py
+++ MAIN/project/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PSC_DB',
+        'USER': 'PSC_DB_user',
+        'PASSWORD': 'PSC_DB_login',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
